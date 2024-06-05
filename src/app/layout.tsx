0fd1dad0fe692e2180b13cb9f27acef62675019d
nest <nsitseo@gmail.com>
2024-06-05 19:54:56 +0530

    base initialize

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  description: `${process.env.SITE_DESCRIPTION}`,
+  title: {
+    default: `${process.env.SITE_NAME}`,
+    template: `%s | ${process.env.SITE_NAME}`
+  }
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
